a9214f22cf08d373d48068ccdb84effd3fb6900b
ryuchan00 <leokun0210@gamil.comn>
2020-04-25 23:53:26 +0900

    Twetter API の利用が面倒すぎる

--- src/app/tweet.service.ts
+++ src/app/tweet.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Tweet } from './tweet';
 import { TWEETS } from './mock-tweets';
 import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 // クラスを 依存関係注入システム に参加するものとしてマークします。
 @Injectable({
@@ -9,7 +10,8 @@ import { Observable, of } from 'rxjs';
 })
 export class TweetService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient) { }
 
   getTweets(): Observable<Tweet[]> {
     return of(TWEETS);
